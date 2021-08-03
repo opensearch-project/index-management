b8e3557118301e958db814f54ee75339a7d506f2
Drew Baugher <46505179+dbbaughe@users.noreply.github.com>
2021-08-03 10:20:50 -0700

    Adds compile only dep on commons codec for digest utils sha1 method in ism rollup

--- src/main/kotlin/org/opensearch/indexmanagement/rollup/model/ISMRollup.kt
+++ src/main/kotlin/org/opensearch/indexmanagement/rollup/model/ISMRollup.kt
@@ -26,6 +26,7 @@
 
 package org.opensearch.indexmanagement.rollup.model
 
+import org.apache.commons.codec.digest.DigestUtils
 import org.opensearch.common.io.stream.StreamInput
 import org.opensearch.common.io.stream.StreamOutput
 import org.opensearch.common.io.stream.Writeable
@@ -41,7 +42,6 @@ import org.opensearch.indexmanagement.common.model.dimension.Histogram
 import org.opensearch.indexmanagement.common.model.dimension.Terms
 import org.opensearch.indexmanagement.util.IndexUtils
 import org.opensearch.jobscheduler.spi.schedule.IntervalSchedule
-import org.opensearch.notification.repackage.org.apache.commons.codec.digest.DigestUtils
 import java.io.IOException
 import java.time.Instant
 import java.time.temporal.ChronoUnit
