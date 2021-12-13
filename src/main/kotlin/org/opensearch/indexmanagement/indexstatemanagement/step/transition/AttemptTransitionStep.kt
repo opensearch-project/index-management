a55703204b61421c5674b17f7fd44f5cf86d2fa3
Ravi Thaluru <thalurur@users.noreply.github.com>
2021-12-13 15:30:42 -0800

    Adding base logic to transition step to enable policy execution
    Signed-off-by: Ravi Thaluru <thalurur@users.noreply.github.com>

--- src/main/kotlin/org/opensearch/indexmanagement/indexstatemanagement/step/transition/AttemptTransitionStep.kt
+++ src/main/kotlin/org/opensearch/indexmanagement/indexstatemanagement/step/transition/AttemptTransitionStep.kt
@@ -5,21 +5,36 @@
 
 package org.opensearch.indexmanagement.indexstatemanagement.step.transition
 
+import org.apache.logging.log4j.LogManager
 import org.opensearch.indexmanagement.spi.indexstatemanagement.Step
 import org.opensearch.indexmanagement.spi.indexstatemanagement.model.ManagedIndexMetaData
+import org.opensearch.indexmanagement.spi.indexstatemanagement.model.StepMetaData
 
 class AttemptTransitionStep : Step(name) {
+
+    private val logger = LogManager.getLogger(javaClass)
+    private var stateName: String? = null
+    private var stepStatus = StepStatus.STARTING
+    private var policyCompleted: Boolean = false
+    private var info: Map<String, Any>? = null
+
     override suspend fun execute(): Step {
-        TODO("Not yet implemented")
+        // TODO: Fix this to actually evaluate the conditions
+        stepStatus = StepStatus.COMPLETED
+        policyCompleted = true
+        return this
     }
 
     override fun getUpdatedManagedIndexMetadata(currentMetadata: ManagedIndexMetaData): ManagedIndexMetaData {
-        TODO("Not yet implemented")
+        return currentMetadata.copy(
+            policyCompleted = policyCompleted,
+            transitionTo = stateName,
+            stepMetaData = StepMetaData(name, getStepStartTime(currentMetadata).toEpochMilli(), stepStatus),
+            info = info
+        )
     }
 
-    override fun isIdempotent(): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun isIdempotent() = true
 
     companion object {
         const val name = "attempt_transition_step"
