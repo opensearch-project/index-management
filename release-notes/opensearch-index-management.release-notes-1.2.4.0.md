75cc8229e7fa22be42c600e9f6db2d309b894949
Clay Downs <89109232+downsrob@users.noreply.github.com>
2022-01-18 13:36:31 -0800

    Disables detekt due to missing dependencies (#250)
    * Disables detekt due to missing dependencies
    
    * 1.2.4 release notes
    
    * fixes flaky rollup/transform explain IT
    
    Signed-off-by: Clay Downs <downsrob@amazon.com>

---
+++ release-notes/opensearch-index-management.release-notes-1.2.4.0.md
@@ -0,0 +1,7 @@
+## Version 1.2.4.0 2022-01-18
+
+Compatible with OpenSearch 1.2.4
+
+### Infrastructure
+
+* Disables detekt due to missing dependencies ([#250](https://github.com/opensearch-project/index-management/pull/250/files))
--- src/test/kotlin/org/opensearch/indexmanagement/transform/resthandler/RestExplainTransformActionIT.kt
+++ src/test/kotlin/org/opensearch/indexmanagement/transform/resthandler/RestExplainTransformActionIT.kt
@@ -73,7 +73,7 @@ class RestExplainTransformActionIT : TransformRestTestCase() {
     @Throws(Exception::class)
     fun `test explain transform for nonexistent id`() {
         // Creating a transform so the config index exists
-        createTransform(transform = randomTransform(), transformId = "doesnt_exist_some_other_id")
+        createTransform(transform = randomTransform(), transformId = "doesnt_exist_some_other_transform_id")
         val response = client().makeRequest("GET", "$TRANSFORM_BASE_URI/doesnt_exist/_explain")
         assertNull("Nonexistent transform didn't return null", response.asMap()["doesnt_exist"])
     }
@@ -81,29 +81,29 @@ class RestExplainTransformActionIT : TransformRestTestCase() {
     @Throws(Exception::class)
     fun `test explain transform for wildcard id`() {
         // Creating a transform so the config index exists
-        createTransform(transform = randomTransform(), transformId = "wildcard_some_id")
-        createTransform(transform = randomTransform(), transformId = "wildcard_some_other_id")
+        createTransform(transform = randomTransform(), transformId = "wildcard_some_transform_id")
+        createTransform(transform = randomTransform(), transformId = "wildcard_some_other_transform_id")
         val response = client().makeRequest("GET", "$TRANSFORM_BASE_URI/wildcard_some*/_explain")
         // We don't expect there to always be metadata we are creating random transforms and the job isn't running
         // but we do expect the wildcard some* to expand to the two jobs created above and have non-null values (meaning they exist)
         val map = response.asMap()
-        assertNotNull("Non null wildcard_some_id value wasn't in the response", map["wildcard_some_id"])
-        assertNotNull("Non null wildcard_some_other_id value wasn't in the response", map["wildcard_some_other_id"])
+        assertNotNull("Non null wildcard_some_transform_id value wasn't in the response", map["wildcard_some_transform_id"])
+        assertNotNull("Non null wildcard_some_other_transform_id value wasn't in the response", map["wildcard_some_other_transform_id"])
     }
 
     @Throws(Exception::class)
     fun `test explain transform for job that hasnt started`() {
-        createTransform(transform = randomTransform().copy(metadataId = null), transformId = "not_started_some_id")
-        val response = client().makeRequest("GET", "$TRANSFORM_BASE_URI/not_started_some_id/_explain")
-        val expectedMap = mapOf("not_started_some_id" to mapOf("metadata_id" to null, "transform_metadata" to null))
+        createTransform(transform = randomTransform().copy(metadataId = null), transformId = "not_started_some_transform_id")
+        val response = client().makeRequest("GET", "$TRANSFORM_BASE_URI/not_started_some_transform_id/_explain")
+        val expectedMap = mapOf("not_started_some_transform_id" to mapOf("metadata_id" to null, "transform_metadata" to null))
         assertEquals("The explain response did not match expected", expectedMap, response.asMap())
     }
 
     @Throws(Exception::class)
     fun `test explain transform for new transform attempted to create with metadata id`() {
-        createTransform(transform = randomTransform().copy(metadataId = "some_metadata_id"), transformId = "no_meta_some_id")
-        val response = client().makeRequest("GET", "$TRANSFORM_BASE_URI/no_meta_some_id/_explain")
-        val expectedMap = mapOf("no_meta_some_id" to mapOf("metadata_id" to null, "transform_metadata" to null))
+        createTransform(transform = randomTransform().copy(metadataId = "some_metadata_id"), transformId = "no_meta_some_transform_id")
+        val response = client().makeRequest("GET", "$TRANSFORM_BASE_URI/no_meta_some_transform_id/_explain")
+        val expectedMap = mapOf("no_meta_some_transform_id" to mapOf("metadata_id" to null, "transform_metadata" to null))
         assertEquals("The explain response did not match expected", expectedMap, response.asMap())
     }
 
