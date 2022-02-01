08db4a8b13c779a10b3a13f65eda96493d9bb5d7
Annie Lee <leeyun@amazon.com>
2022-02-01 15:10:33 -0800

    Update IndexPriorityAction.kt
    Signed-off-by: Annie Lee <leeyun@amazon.com>

--- src/main/kotlin/org/opensearch/indexmanagement/indexstatemanagement/action/IndexPriorityAction.kt
+++ src/main/kotlin/org/opensearch/indexmanagement/indexstatemanagement/action/IndexPriorityAction.kt
@@ -5,6 +5,9 @@
 
 package org.opensearch.indexmanagement.indexstatemanagement.action
 
+import org.opensearch.common.io.stream.StreamOutput
+import org.opensearch.common.xcontent.ToXContent
+import org.opensearch.common.xcontent.XContentBuilder
 import org.opensearch.indexmanagement.indexstatemanagement.step.indexpriority.AttemptSetIndexPriorityStep
 import org.opensearch.indexmanagement.spi.indexstatemanagement.Action
 import org.opensearch.indexmanagement.spi.indexstatemanagement.Step
@@ -25,6 +28,17 @@ class IndexPriorityAction(
     override fun getStepToExecute(context: StepContext): Step = attemptSetIndexPriorityStep
 
     override fun getSteps(): List<Step> = steps
+
+    override fun populateAction(builder: XContentBuilder, params: ToXContent.Params) {
+        builder.startObject(type)
+        builder.field(INDEX_PRIORITY_FIELD, indexPriority)
+        builder.endObject()
+    }
+
+    override fun populateAction(out: StreamOutput) {
+        out.writeInt(indexPriority)
+        out.writeInt(actionIndex)
+    }
 
     companion object {
         const val name = "index_priority"
