34d1868b24e895ded9101341a2b2a6190ce95aa2
Petar Dzepina <petar.dzepina@gmail.com>
2022-11-03 01:29:13 +0100

    alias in  rollup target_index field (#445)
    * added support for mustache scripting of rollup.target_index field (#435)
    * tests
    * small refactor/improvements
    * added wildcard check when creating rollup job; removed resolving targetIndex on Rollup init; added test for wildcards
    * lint fixes
    * moved target_index validation in getRollup resp handler
    * removed catch block
    * fixed IT fail
    * added Exception catch block
    
    Signed-off-by: Petar Dzepina <petar.dzepina@gmail.com>
    (cherry picked from commit 70cf4eadb28571b14da5252e7c7d98aade2867d5)

--- src/test/kotlin/org/opensearch/indexmanagement/rollup/util/RollupFieldValueExpressionResolverTests.kt
+++ src/test/kotlin/org/opensearch/indexmanagement/rollup/util/RollupFieldValueExpressionResolverTests.kt
@@ -11,6 +11,8 @@ import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
 import com.nhaarman.mockitokotlin2.doReturn
 import org.junit.Before
+import org.mockito.ArgumentMatchers.anyString
+import org.opensearch.cluster.service.ClusterService
 import org.opensearch.indexmanagement.rollup.randomRollup
 import org.opensearch.ingest.TestTemplateService
 import org.opensearch.script.ScriptService
@@ -20,21 +22,35 @@ import org.opensearch.test.OpenSearchTestCase
 class RollupFieldValueExpressionResolverTests : OpenSearchTestCase() {
 
     private val scriptService: ScriptService = mock()
+    private val clusterService: ClusterService = mock()
+    private var indexAliasUtils: RollupFieldValueExpressionResolver.IndexAliasUtils = mock()
 
     @Before
     fun settings() {
-        RollupFieldValueExpressionResolver.registerScriptService(scriptService)
+        this.indexAliasUtils = mock()
+        RollupFieldValueExpressionResolver.registerServices(scriptService, clusterService, indexAliasUtils)
     }
 
-    fun `test resolving successfully`() {
+    fun `test resolving no alias successfully`() {
         whenever(scriptService.compile(any(), eq(TemplateScript.CONTEXT))).doReturn(TestTemplateService.MockTemplateScript.Factory("test_index_123"))
+        whenever(indexAliasUtils.isAlias(anyString())).doReturn(false)
         val rollup = randomRollup().copy(sourceIndex = "test_index_123", targetIndex = "{{ctx.source_index}}")
         val targetIndexResolvedName = RollupFieldValueExpressionResolver.resolve(rollup, rollup.targetIndex)
         assertEquals("test_index_123", targetIndexResolvedName)
     }
 
+    fun `test resolving with alias successfully`() {
+        whenever(scriptService.compile(any(), eq(TemplateScript.CONTEXT))).doReturn(TestTemplateService.MockTemplateScript.Factory("test_index_123"))
+        whenever(indexAliasUtils.isAlias(anyString())).doReturn(true)
+        whenever(indexAliasUtils.getWriteIndexNameForAlias(anyString())).doReturn("backing_index")
+        val rollup = randomRollup().copy(sourceIndex = "test_index_123", targetIndex = "{{ctx.source_index}}")
+        val targetIndexResolvedName = RollupFieldValueExpressionResolver.resolve(rollup, rollup.targetIndex)
+        assertEquals("backing_index", targetIndexResolvedName)
+    }
+
     fun `test resolving failed returned passed value`() {
         whenever(scriptService.compile(any(), eq(TemplateScript.CONTEXT))).doReturn(TestTemplateService.MockTemplateScript.Factory(""))
+        whenever(indexAliasUtils.isAlias(anyString())).doReturn(false)
         val rollup = randomRollup().copy(sourceIndex = "test_index_123", targetIndex = "{{ctx.source_index}}")
         val targetIndexResolvedName = RollupFieldValueExpressionResolver.resolve(rollup, rollup.targetIndex)
         assertEquals("{{ctx.source_index}}", targetIndexResolvedName)
