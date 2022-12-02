e34acd1f1f136464856322c38e9e04a4967e55b2
Petar Dzepina <petar.dzepina@vroom.com>
2022-12-02 03:17:31 +0100

    ktlint fix
    Signed-off-by: Petar Dzepina <petar.dzepina@vroom.com>

--- src/main/kotlin/org/opensearch/indexmanagement/rollup/interceptor/RollupInterceptor.kt
+++ src/main/kotlin/org/opensearch/indexmanagement/rollup/interceptor/RollupInterceptor.kt
@@ -163,7 +163,7 @@ class RollupInterceptor(
         return fieldMappings
     }
 
-    @Suppress("ComplexMethod","ThrowsCount")
+    @Suppress("ComplexMethod", "ThrowsCount")
     private fun getQueryMetadata(
         query: QueryBuilder?,
         concreteSourceIndexName: String,
