3491927523fd52c7be900a0345fc67e014ade862
Angie Zhang <langelzh@amazon.com>
2023-04-06 11:26:05 -0700

    Error Prevention: Add close action
    Signed-off-by: Angie Zhang <langelzh@amazon.com>

--- src/main/kotlin/org/opensearch/indexmanagement/indexstatemanagement/validation/ActionValidation.kt
+++ src/main/kotlin/org/opensearch/indexmanagement/indexstatemanagement/validation/ActionValidation.kt
@@ -27,7 +27,7 @@ class ActionValidation(
             "open" -> ValidateOpen(settings, clusterService, jvmService).execute(indexName)
             "read_only" -> ValidateReadOnly(settings, clusterService, jvmService).execute(indexName)
             "read_write" -> ValidateReadWrite(settings, clusterService, jvmService).execute(indexName)
-            "replica_count" -> ValidateReplicaCount(settings, clusterService, jvmService).execute(indexName)
+            "close" -> ValidateClose(settings, clusterService, jvmService).execute(indexName)"replica_count" -> ValidateReplicaCount(settings, clusterService, jvmService).execute(indexName)
             else -> {
                 // temporary call until all actions are mapped
                 ValidateNothing(settings, clusterService, jvmService).execute(indexName)
