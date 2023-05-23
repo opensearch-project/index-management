49716047b63a1c001b8316f543e049df292ededd
Hailong Cui <ihailong@amazon.com>
2023-05-23 10:41:00 +0800

    Fix build errors after refactoring of Strings class in core (#786)
    (cherry picked from commit 30e16d999d18e0001c8963794f8fcef5eb3c6e80)
    Signed-off-by: Hailong Cui <ihailong@amazon.com>

--- src/main/kotlin/org/opensearch/indexmanagement/indexstatemanagement/resthandler/RestRetryFailedManagedIndexAction.kt
+++ src/main/kotlin/org/opensearch/indexmanagement/indexstatemanagement/resthandler/RestRetryFailedManagedIndexAction.kt
@@ -6,7 +6,7 @@
 package org.opensearch.indexmanagement.indexstatemanagement.resthandler
 
 import org.opensearch.client.node.NodeClient
-import org.opensearch.common.Strings
+import org.opensearch.core.common.Strings
 import org.opensearch.common.logging.DeprecationLogger
 import org.opensearch.common.xcontent.XContentHelper
 import org.opensearch.core.xcontent.MediaType
