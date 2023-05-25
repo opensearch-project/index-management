04bf664b88ced09a81b52576d72c35f706625f34
zhichao-aws <zhichaog@amazon.com>
2023-05-25 17:29:49 +0800

    [Backport 2.x] CRUD api for long running operation notification config (#791)
    * CRUD api for long running operation notification config (#722)
    
    * notification crud api
    
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>
    
    * changes based on comments; add tests
    
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>
    
    * changes for comment
    
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>
    
    * change behavior for get action index not exists, fix test cases
    
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>
    
    * fix weak password for changes in security plugin
    
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>
    
    ---------
    
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>
    (cherry picked from commit 96f7380341af6e978081a7568e33dbf9cd58f7e3)
    
    * fix test code apache http package version
    
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>
    
    ---------
    
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>
    Signed-off-by: Ronnak Saxena <ronsax@amazon.com>

---
+++ src/main/kotlin/org/opensearch/indexmanagement/controlcenter/notification/action/index/IndexLRONConfigAction.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.indexmanagement.controlcenter.notification.action.index
+
+import org.opensearch.action.ActionType
+import org.opensearch.indexmanagement.controlcenter.notification.LRONConfigResponse
+
+class IndexLRONConfigAction private constructor() :
+    ActionType<LRONConfigResponse>(NAME, ::LRONConfigResponse) {
+    companion object {
+        val INSTANCE = IndexLRONConfigAction()
+        const val NAME = "cluster:admin/opensearch/controlcenter/lron/write"
+    }
+}
---
+++ src/test/kotlin/org/opensearch/indexmanagement/controlcenter/notification/resthandler/LRONConfigRestTestCase.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.indexmanagement.controlcenter.notification.resthandler
+
+import org.apache.http.HttpEntity
+import org.apache.http.entity.ContentType
+import org.apache.http.entity.StringEntity
+import org.junit.After
+import org.junit.AfterClass
+import org.junit.Before
+import org.opensearch.client.Response
+import org.opensearch.client.ResponseException
+import org.opensearch.indexmanagement.IndexManagementPlugin
+import org.opensearch.indexmanagement.IndexManagementRestTestCase
+import org.opensearch.indexmanagement.controlcenter.notification.initNodeIdsInRestIT
+import org.opensearch.indexmanagement.controlcenter.notification.model.LRONConfig
+import org.opensearch.indexmanagement.controlcenter.notification.nodeIdsInRestIT
+import org.opensearch.indexmanagement.controlcenter.notification.randomLRONConfig
+import org.opensearch.indexmanagement.controlcenter.notification.randomTaskId
+import org.opensearch.indexmanagement.controlcenter.notification.toJsonString
+import org.opensearch.indexmanagement.makeRequest
+import org.opensearch.rest.RestStatus
+
+abstract class LRONConfigRestTestCase : IndexManagementRestTestCase() {
+    @Before
+    fun prepareForIT() {
+        setDebugLogLevel()
+        /* init cluster node ids in integ test */
+        initNodeIdsInRestIT(client())
+        /* index a random doc to create .opensearch-control-center index */
+        createLRONConfig(randomLRONConfig(taskId = randomTaskId(nodeId = nodeIdsInRestIT.random())))
+    }
+
+    @After
+    fun removeAllDocs() {
+        try {
+            client().makeRequest(
+                "POST",
+                "${IndexManagementPlugin.CONTROL_CENTER_INDEX}/_delete_by_query",
+                mapOf("refresh" to "true"),
+                StringEntity("""{"query": {"match_all": {}}}""", ContentType.APPLICATION_JSON)
+            )
+        } catch (e: ResponseException) {
+            logger.info(e.response.asMap())
+            /* ignore if the index has not been created */
+            assertEquals("Unexpected status", RestStatus.NOT_FOUND, e.response.restStatus())
+        }
+    }
+
+    fun createLRONConfig(lronConfig: LRONConfig): Response {
+        return client().makeRequest("POST", IndexManagementPlugin.LRON_BASE_URI, emptyMap(), lronConfig.toHttpEntity())
+    }
+
+    private fun setDebugLogLevel() {
+        client().makeRequest(
+            "PUT", "_cluster/settings",
+            StringEntity(
+                """
+                {
+                    "transient": {
+                        "logger.org.opensearch.indexmanagement.controlcenter.notification":"DEBUG"
+                    }
+                }
+                """.trimIndent(),
+                ContentType.APPLICATION_JSON
+            )
+        )
+    }
+
+    protected fun LRONConfig.toHttpEntity(): HttpEntity = StringEntity(toJsonString(), ContentType.APPLICATION_JSON)
+
+    companion object {
+        @AfterClass
+        @JvmStatic fun clearIndicesAfterClass() {
+            wipeAllIndices()
+        }
+    }
+}
