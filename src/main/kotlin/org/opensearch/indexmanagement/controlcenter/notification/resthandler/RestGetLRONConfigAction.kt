d2d11c0cd2645a25511be30abf3dfa6a0d2f558e
zhichao-aws <zhichaog@amazon.com>
2023-05-25 15:51:37 +0800

    CRUD api for long running operation notification config (#722)
    * notification crud api
    
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>
    
    * changes based on comments; add tests
    
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>
    
    * changes for comment
    
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>
    
    * change behavior for get action index not exists, fix test cases
    
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>
    
    * fix weak password for changes in security plugin
    
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>
    
    ---------
    
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>
    (cherry picked from commit 96f7380341af6e978081a7568e33dbf9cd58f7e3)

---
+++ src/main/kotlin/org/opensearch/indexmanagement/controlcenter/notification/resthandler/RestGetLRONConfigAction.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.indexmanagement.controlcenter.notification.resthandler
+import org.opensearch.client.node.NodeClient
+import org.opensearch.indexmanagement.IndexManagementPlugin
+import org.opensearch.indexmanagement.controlcenter.notification.action.get.GetLRONConfigAction
+import org.opensearch.indexmanagement.controlcenter.notification.action.get.GetLRONConfigRequest
+import org.opensearch.indexmanagement.controlcenter.notification.util.DEFAULT_LRON_CONFIG_SORT_FIELD
+import org.opensearch.rest.BaseRestHandler
+import org.opensearch.rest.BaseRestHandler.RestChannelConsumer
+import org.opensearch.rest.RestRequest
+import org.opensearch.indexmanagement.util.getSearchParams
+import org.opensearch.rest.RestHandler
+import org.opensearch.rest.action.RestToXContentListener
+import java.io.IOException
+
+class RestGetLRONConfigAction : BaseRestHandler() {
+    override fun routes(): List<RestHandler.Route> {
+        return listOf(
+            RestHandler.Route(RestRequest.Method.GET, IndexManagementPlugin.LRON_BASE_URI),
+            RestHandler.Route(RestRequest.Method.GET, "${IndexManagementPlugin.LRON_BASE_URI}/{id}")
+        )
+    }
+
+    override fun getName(): String {
+        return "get_lron_config_action"
+    }
+
+    @Throws(IOException::class)
+    override fun prepareRequest(request: RestRequest, client: NodeClient): RestChannelConsumer {
+        val docId = request.param("id")
+        val searchParams = if (null == docId) request.getSearchParams(DEFAULT_LRON_CONFIG_SORT_FIELD) else null
+        val getLRONConfigRequest = GetLRONConfigRequest(docId, searchParams)
+
+        return RestChannelConsumer { channel ->
+            client.execute(GetLRONConfigAction.INSTANCE, getLRONConfigRequest, RestToXContentListener(channel))
+        }
+    }
+}
