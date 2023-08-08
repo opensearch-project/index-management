c9c8860b35486db55ca29b71db135f1af5020550
Hailong Cui <ihailong@amazon.com>
2023-08-08 23:26:29 +0800

    core refactor change (#887)
    Signed-off-by: Hailong Cui <ihailong@amazon.com>

--- spi/src/main/kotlin/org.opensearch.indexmanagement.spi/indexstatemanagement/model/StepMetaData.kt
+++ spi/src/main/kotlin/org.opensearch.indexmanagement.spi/indexstatemanagement/model/StepMetaData.kt
@@ -5,7 +5,7 @@
 
 package org.opensearch.indexmanagement.spi.indexstatemanagement.model
 
-import org.opensearch.common.Strings
+import org.opensearch.core.common.Strings
 import org.opensearch.core.common.io.stream.StreamInput
 import org.opensearch.core.common.io.stream.StreamOutput
 import org.opensearch.core.common.io.stream.Writeable
