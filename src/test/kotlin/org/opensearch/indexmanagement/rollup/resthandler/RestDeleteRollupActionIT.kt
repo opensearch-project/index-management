495ee4d0c2f05590b116535bd76c7d52dd116d49
Ronnak Saxena <ronsax@amazon.com>
2023-09-06 17:57:52 +0000

    deleted rest test
    Signed-off-by: Ronnak Saxena <ronsax@amazon.com>

--- src/test/kotlin/org/opensearch/indexmanagement/rollup/resthandler/RestDeleteRollupActionIT.kt
+++ src/test/kotlin/org/opensearch/indexmanagement/rollup/resthandler/RestDeleteRollupActionIT.kt
@@ -6,7 +6,6 @@
 package org.opensearch.indexmanagement.rollup.resthandler
 
 import org.opensearch.client.ResponseException
-import org.opensearch.indexmanagement.IndexManagementPlugin.Companion.INDEX_MANAGEMENT_INDEX
 import org.opensearch.indexmanagement.IndexManagementPlugin.Companion.ROLLUP_JOBS_BASE_URI
 import org.opensearch.indexmanagement.makeRequest
 import org.opensearch.indexmanagement.rollup.RollupRestTestCase
@@ -31,17 +30,6 @@ class RestDeleteRollupActionIT : RollupRestTestCase() {
     fun `test deleting a rollup that doesn't exist in existing config index`() {
         try {
             createRandomRollup()
-            client().makeRequest("DELETE", "$ROLLUP_JOBS_BASE_URI/foobarbaz")
-            fail("expected 404 ResponseException")
-        } catch (e: ResponseException) {
-            assertEquals(RestStatus.NOT_FOUND, e.response.restStatus())
-        }
-    }
-
-    @Throws(Exception::class)
-    fun `test deleting a rollup that doesn't exist and config index doesnt exist`() {
-        try {
-            deleteIndex(INDEX_MANAGEMENT_INDEX)
             client().makeRequest("DELETE", "$ROLLUP_JOBS_BASE_URI/foobarbaz")
             fail("expected 404 ResponseException")
         } catch (e: ResponseException) {
