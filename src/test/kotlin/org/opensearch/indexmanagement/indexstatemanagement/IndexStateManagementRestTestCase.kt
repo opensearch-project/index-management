ac6b791190b5ef8d367d2d4e1c5db8519712ea79
Joshua152 <59714582+Joshua152@users.noreply.github.com>
2023-12-19 12:50:50 -0500

    Issue651 tests (#1046)
    * Implemented filtering on the ISM eplain API
    
    Signed-off-by: Joshua Au <joshuahyau@gmail.com>
    
    * Fixed tests for ExplainRequest
    
    Signed-off-by: Joshua Au <joshuahyau@gmail.com>
    
    * Added filtering on query and metadata map
    
    Signed-off-by: Joshua Au <joshuahyau@gmail.com>
    
    * Filtered on indexNames in metadata
    
    Signed-off-by: Joshua Au <joshuahyau@gmail.com>
    
    * Fixed github workflow check errors
    
    Signed-off-by: Joshua Au <joshuahyau@gmail.com>
    
    * Removed debugging comments
    
    Signed-off-by: Joshua Au <joshuahyau@gmail.com>
    
    * Updated code styling to make more clear
    
    Signed-off-by: Joshua Au <joshuahyau@gmail.com>
    
    * Refactored code to match suggestions
    
    Signed-off-by: Joshua Au <joshuahyau@gmail.com>
    
    * Added test case for the ExplainFilter.byMetaData and parse methods
    
    Signed-off-by: Joshua Au <joshuahyau@gmail.com>
    
    * Started implementation of explain filter IT
    
    Signed-off-by: Joshua Au <joshuahyau@gmail.com>
    
    * Implemented test explain filter method
    
    Signed-off-by: Joshua Au <joshuahyau@gmail.com>
    
    * Implemented explain filter test on failure
    
    Signed-off-by: Joshua Au <joshuahyau@gmail.com>
    
    * Cleaned up log statements
    
    Signed-off-by: Joshua Au <joshuahyau@gmail.com>
    
    * Added explain filter test for success
    
    Signed-off-by: Joshua Au <joshuahyau@gmail.com>
    
    * Fixed lint errors
    
    Signed-off-by: Joshua Au <joshuahyau@gmail.com>
    
    * Removed policy from index to fix flaky tests
    
    Signed-off-by: Joshua Au <joshuahyau@gmail.com>
    
    ---------
    
    Signed-off-by: Joshua Au <joshuahyau@gmail.com>
    Signed-off-by: bowenlan-amzn <bowenlan23@gmail.com>
    Co-authored-by: bowenlan-amzn <bowenlan23@gmail.com>

--- src/test/kotlin/org/opensearch/indexmanagement/indexstatemanagement/IndexStateManagementRestTestCase.kt
+++ src/test/kotlin/org/opensearch/indexmanagement/indexstatemanagement/IndexStateManagementRestTestCase.kt
@@ -476,6 +476,8 @@ abstract class IndexStateManagementRestTestCase : IndexManagementRestTestCase() 
 
     protected fun ExplainFilter.toHttpEntity(): HttpEntity = StringEntity(toJsonString(), APPLICATION_JSON)
 
+    protected fun ExplainFilter.toHttpEntity(): HttpEntity = StringEntity(toJsonString(), ContentType.APPLICATION_JSON)
+
     protected fun ChangePolicy.toHttpEntity(): HttpEntity {
         var string = "{\"${ChangePolicy.POLICY_ID_FIELD}\":\"$policyID\","
         if (state != null) {
