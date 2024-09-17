e5fde6f193c3e4c7e399f58df1467db9c0eaba6a
Kshitij Tandon <tandonks@amazon.com>
2024-09-17 17:41:00 +0530

    Fixing snapshot bug (#1257)
    Signed-off-by: Kshitij Tandon <tandonks@amazon.com>
    Co-authored-by: bowenlan-amzn <bowenlan23@gmail.com>
    (cherry picked from commit 809f85ca2fc26c7b1ee1fd3060bd35bf56b46520)

--- src/main/kotlin/org/opensearch/indexmanagement/indexstatemanagement/step/snapshot/WaitForSnapshotStep.kt
+++ src/main/kotlin/org/opensearch/indexmanagement/indexstatemanagement/step/snapshot/WaitForSnapshotStep.kt
@@ -7,20 +7,19 @@ package org.opensearch.indexmanagement.indexstatemanagement.step.snapshot
 
 import org.apache.logging.log4j.LogManager
 import org.opensearch.ExceptionsHelper
-import org.opensearch.action.admin.cluster.snapshots.status.SnapshotStatus
-import org.opensearch.action.admin.cluster.snapshots.status.SnapshotsStatusRequest
-import org.opensearch.action.admin.cluster.snapshots.status.SnapshotsStatusResponse
-import org.opensearch.cluster.SnapshotsInProgress.State
+import org.opensearch.action.admin.cluster.snapshots.get.GetSnapshotsRequest
+import org.opensearch.action.admin.cluster.snapshots.get.GetSnapshotsResponse
 import org.opensearch.indexmanagement.indexstatemanagement.action.SnapshotAction
 import org.opensearch.indexmanagement.opensearchapi.suspendUntil
 import org.opensearch.indexmanagement.spi.indexstatemanagement.Step
 import org.opensearch.indexmanagement.spi.indexstatemanagement.model.ActionProperties
 import org.opensearch.indexmanagement.spi.indexstatemanagement.model.ManagedIndexMetaData
 import org.opensearch.indexmanagement.spi.indexstatemanagement.model.StepMetaData
+import org.opensearch.snapshots.SnapshotInfo
+import org.opensearch.snapshots.SnapshotState
 import org.opensearch.transport.RemoteTransportException
 
 class WaitForSnapshotStep(private val action: SnapshotAction) : Step(name) {
-
     private val logger = LogManager.getLogger(javaClass)
     private var stepStatus = StepStatus.STARTING
     private var info: Map<String, Any>? = null
@@ -34,30 +33,31 @@ class WaitForSnapshotStep(private val action: SnapshotAction) : Step(name) {
 
         try {
             val snapshotName = getSnapshotName(managedIndexMetadata, indexName) ?: return this
-            val request = SnapshotsStatusRequest()
+            val newRequest = GetSnapshotsRequest()
                 .snapshots(arrayOf(snapshotName))
                 .repository(repository)
-            val response: SnapshotsStatusResponse = context.client.admin().cluster().suspendUntil { snapshotsStatus(request, it) }
-            val status: SnapshotStatus? = response
-                .snapshots
-                .find { snapshotStatus ->
-                    snapshotStatus.snapshot.snapshotId.name == snapshotName && snapshotStatus.snapshot.repository == repository
-                }
-            if (status != null) {
-                when (status.state) {
-                    State.INIT, State.STARTED -> {
-                        stepStatus = StepStatus.CONDITION_NOT_MET
-                        info = mapOf("message" to getSnapshotInProgressMessage(indexName), "state" to status.state.name)
+            val response: GetSnapshotsResponse = context.client.admin().cluster().suspendUntil { getSnapshots(newRequest, it) }
+            val status: SnapshotInfo? =
+                response
+                    .snapshots
+                    .find { snapshotInfo ->
+                        snapshotInfo.snapshotId().name == snapshotName
                     }
-                    State.SUCCESS -> {
+            if (status != null) {
+                when (status.state()) {
+                    SnapshotState.IN_PROGRESS -> {
+                        stepStatus = StepStatus.CONDITION_NOT_MET
+                        info = mapOf("message" to getSnapshotInProgressMessage(indexName), "state" to status.state().toString())
+                    }
+                    SnapshotState.SUCCESS -> {
                         stepStatus = StepStatus.COMPLETED
-                        info = mapOf("message" to getSuccessMessage(indexName), "state" to status.state.name)
+                        info = mapOf("message" to getSuccessMessage(indexName), "state" to status.state().toString())
                     }
                     else -> { // State.FAILED, State.ABORTED
                         val message = getFailedExistsMessage(indexName)
                         logger.warn(message)
                         stepStatus = StepStatus.FAILED
-                        info = mapOf("message" to message, "state" to status.state.name)
+                        info = mapOf("message" to message, "state" to status.state().toString())
                     }
                 }
             } else {
@@ -97,23 +97,26 @@ class WaitForSnapshotStep(private val action: SnapshotAction) : Step(name) {
         return actionProperties.snapshotName
     }
 
-    override fun getUpdatedManagedIndexMetadata(currentMetadata: ManagedIndexMetaData): ManagedIndexMetaData {
-        return currentMetadata.copy(
-            stepMetaData = StepMetaData(name, getStepStartTime(currentMetadata).toEpochMilli(), stepStatus),
-            transitionTo = null,
-            info = info,
-        )
-    }
+    override fun getUpdatedManagedIndexMetadata(currentMetadata: ManagedIndexMetaData): ManagedIndexMetaData = currentMetadata.copy(
+        stepMetaData = StepMetaData(name, getStepStartTime(currentMetadata).toEpochMilli(), stepStatus),
+        transitionTo = null,
+        info = info,
+    )
 
     override fun isIdempotent(): Boolean = true
 
     companion object {
         const val name = "wait_for_snapshot"
+
         fun getFailedMessage(index: String) = "Failed to get status of snapshot [index=$index]"
+
         fun getFailedExistsMessage(index: String) = "Snapshot doesn't exist [index=$index]"
+
         fun getFailedActionPropertiesMessage(index: String, actionProperties: ActionProperties?) =
             "Unable to retrieve [${ActionProperties.Properties.SNAPSHOT_NAME.key}] from ActionProperties=$actionProperties [index=$index]"
+
         fun getSuccessMessage(index: String) = "Successfully created snapshot [index=$index]"
+
         fun getSnapshotInProgressMessage(index: String) = "Snapshot currently in progress [index=$index]"
     }
 }
