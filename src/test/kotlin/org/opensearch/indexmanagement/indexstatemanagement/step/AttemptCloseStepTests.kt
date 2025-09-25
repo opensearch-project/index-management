dde6210ff6d9a7f1d4b421e4f336106f9ddd2cb5
bowenlan-amzn <bowenlan23@gmail.com>
2025-09-25 22:47:09 +0800

    fix compile
    Signed-off-by: bowenlan-amzn <bowenlan23@gmail.com>

--- src/test/kotlin/org/opensearch/indexmanagement/indexstatemanagement/step/AttemptCloseStepTests.kt
+++ src/test/kotlin/org/opensearch/indexmanagement/indexstatemanagement/step/AttemptCloseStepTests.kt
@@ -45,7 +45,7 @@ class AttemptCloseStepTests : OpenSearchTestCase() {
     private val clusterService: ClusterService = mock()
     private val scriptService: ScriptService = mock()
     private val settings: Settings = Settings.EMPTY
-    private val lockService: LockService = LockService(mock(), clusterService)
+    private val lockService: LockService = mock()
     private lateinit var metricsRegistry: MetricsRegistry
     private lateinit var closeActionMetrics: CloseActionMetrics
 
