39b856d900b420f4768cfc40d2788ef32c410d9b
Tarun Kishore <75606327+Tarun-kishore@users.noreply.github.com>
2025-11-17 10:45:07 +0530

    Use optionalField for creation in ExplainSMPolicy serialization (#1507)
    Co-authored-by: Tarun-kishore <Tarun-kishore@users.noreply.github.com>
    Co-authored-by: bowenlan-amzn <bowenlan23@gmail.com>

--- src/test/kotlin/org/opensearch/indexmanagement/snapshotmanagement/action/ResponseTests.kt
+++ src/test/kotlin/org/opensearch/indexmanagement/snapshotmanagement/action/ResponseTests.kt
@@ -6,9 +6,12 @@
 package org.opensearch.indexmanagement.snapshotmanagement.action
 
 import org.opensearch.common.io.stream.BytesStreamOutput
+import org.opensearch.common.xcontent.XContentFactory
 import org.opensearch.core.common.io.stream.StreamInput
 import org.opensearch.core.rest.RestStatus
+import org.opensearch.core.xcontent.ToXContent
 import org.opensearch.indexmanagement.indexstatemanagement.util.XCONTENT_WITHOUT_TYPE_AND_USER
+import org.opensearch.indexmanagement.opensearchapi.toMap
 import org.opensearch.indexmanagement.snapshotmanagement.api.transport.explain.ExplainSMPolicyResponse
 import org.opensearch.indexmanagement.snapshotmanagement.api.transport.get.GetSMPoliciesResponse
 import org.opensearch.indexmanagement.snapshotmanagement.api.transport.get.GetSMPolicyResponse
@@ -17,8 +20,8 @@ import org.opensearch.indexmanagement.snapshotmanagement.model.ExplainSMPolicy
 import org.opensearch.indexmanagement.snapshotmanagement.randomSMMetadata
 import org.opensearch.indexmanagement.snapshotmanagement.randomSMPolicy
 import org.opensearch.indexmanagement.snapshotmanagement.smDocIdToPolicyName
-import org.opensearch.indexmanagement.snapshotmanagement.toMap
 import org.opensearch.test.OpenSearchTestCase
+import org.opensearch.indexmanagement.snapshotmanagement.toMap as toMapHelper
 
 class ResponseTests : OpenSearchTestCase() {
     fun `test index sm policy response`() {
@@ -38,8 +41,8 @@ class ResponseTests : OpenSearchTestCase() {
     fun `test index sm policy toXContent`() {
         val smPolicy = randomSMPolicy()
         val res = IndexSMPolicyResponse("someid", 1L, 2L, 3L, smPolicy, RestStatus.OK)
-        val resMap = res.toMap()
-        assertEquals(resMap["sm_policy"], smPolicy.toMap(XCONTENT_WITHOUT_TYPE_AND_USER))
+        val resMap = res.toMapHelper()
+        assertEquals(resMap["sm_policy"], smPolicy.toMapHelper(XCONTENT_WITHOUT_TYPE_AND_USER))
     }
 
     fun `test get sm policy response`() {
@@ -81,7 +84,32 @@ class ResponseTests : OpenSearchTestCase() {
     fun `test get sm policy toXContent`() {
         val smPolicy = randomSMPolicy()
         val res = GetSMPolicyResponse("someid", 1L, 2L, 3L, smPolicy)
-        val resMap = res.toMap()
-        assertEquals(resMap["sm_policy"], smPolicy.toMap(XCONTENT_WITHOUT_TYPE_AND_USER))
+        val resMap = res.toMapHelper()
+        assertEquals(resMap["sm_policy"], smPolicy.toMapHelper(XCONTENT_WITHOUT_TYPE_AND_USER))
+    }
+
+    fun `test explain sm policy toXContent with null creation`() {
+        // Test that ExplainSMPolicy.toXContent() handles null creation correctly (for V_3_3_0+)
+        val smMetadata = randomSMMetadata()
+        val smMetadataWithNullCreation = smMetadata.copy(creation = null)
+        val explainSMPolicy = ExplainSMPolicy(smMetadataWithNullCreation, true)
+
+        // Properly convert ToXContentFragment to map by wrapping in an object
+        val builder = XContentFactory.jsonBuilder().startObject()
+        explainSMPolicy.toXContent(builder, ToXContent.EMPTY_PARAMS)
+        builder.endObject()
+        val explainMap = builder.toMap()
+
+        // Verify that creation field is NOT present when null (optionalField behavior for V_3_3_0+)
+        assertFalse("Creation field should not be present when null", explainMap.containsKey("creation"))
+
+        // Verify deletion field IS present
+        assertTrue("Deletion field should be present", explainMap.containsKey("deletion"))
+
+        // Verify other mandatory fields are present
+        assertTrue("Policy seq_no should be present", explainMap.containsKey("policy_seq_no"))
+        assertTrue("Policy primary_term should be present", explainMap.containsKey("policy_primary_term"))
+        assertTrue("Enabled field should be present", explainMap.containsKey("enabled"))
+        assertEquals(true, explainMap["enabled"])
     }
 }
